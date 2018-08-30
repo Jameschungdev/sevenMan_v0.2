0bed36918f4e19f7023588c7ac42f1a9420743f0
Jameschungdev <42752695+Jameschungdev@users.noreply.github.com>
2018-08-30 17:33:42 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -20,28 +20,66 @@ app.use('/client',express.static(__dirname+'/client'));//if request matches /cli
 serv.listen(2000); //listening for request for port 2000. Use localhost:2000 for domain for local testing.
 console.log("Server started."); //states success when server starts when cmd node app.js
 
+//List of client connections
+var CLIENT_LIST = {};
+var clientNo = 0;
+
 //(R1) loads socket.io and initialises it.
 var io = require('socket.io')(serv,{});
-
 //whenever a user connects, this function(socket) runs.
-io.sockets.on('connection',function(socket){ 
-    console.log('Socket connection successful.');
+io.sockets.on('connection',function(client){ 
+    clientNo++; //increment client number to differentiate
+    client.id = clientNo;
+    client.x = 0;
+    client.y = 0;
+    client.no = client.id; //can be replaced with name in future.
+    CLIENT_LIST[clientNo] = client;
+    console.log('Client '+clientNo+' connection successful.');
 
+    //Remove player when disconnecting
+    client.on('disconnect',function(){
+        delete CLIENT_LIST[client.id];
+    });
+
+    /* //Example of server listening and sending
     //Server sends a message
     var hello = function(){
-        socket.emit('serverMsg',{ //output label
+        client.emit('serverMsg',{ //output label
             msg:'hello from server' //object label msg from server
         }); 
     }
 
     //Server listens for message
-    socket.on('happy',function(input){ //listen for client 'happy', execute. 
+    client.on('happy',function(input){ //listen for client 'happy', execute. 
         console.log('Client says happy because he inputted '+input.reason);
         hello();
     });
-
-    
+    */
 });
+
+//loop for canvas/game drawing
+setInterval(function(){ //for every 40ms/ every frame...
+    var pack = []; //create a new clean package of data to send out every frame
+
+    //calculate and put into package
+    for(var i in CLIENT_LIST){ //increment positions
+        var client = CLIENT_LIST[i];
+        client.x ++;
+        client.y ++;
+
+        pack.push({ //push data of new position into packet
+            x:client.x,
+            y:client.y,
+            no:client.no,
+        });
+    }
+
+    for (var i in CLIENT_LIST){
+        var client = CLIENT_LIST[i];
+        client.emit('newPositions',pack); //must send package to each player connected to server
+    }
+    
+},1000/25);
 
 
 //cont. 4:26
